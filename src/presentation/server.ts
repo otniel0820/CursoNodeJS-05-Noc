7a5146b849102abb29ffd671cd0dbc44af44aa76
Otniel Lascano dev <otniel.lascano.dev@proton.me>
2024-01-01 04:08:56 +0100

    Cron and CheckServiceUseCase

--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -1,10 +1,16 @@
+import { CheckService } from "../domain/useCases/checks/checksService";
+import { CronService } from "./cron/cronService";
 
 
 
 export class Server{
     public static start(){
         console.log('Server started...');
-        
+        CronService.createJob('*/5 * * * * *',()=>{
+            new CheckService().execute('https://www.google.com')
+            
+        })
+       
     }
 }
 
