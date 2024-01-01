38f0eebd0c3e41ab47d26f8e44ddd34219d8ca25
Otniel Lascano dev <otniel.lascano.dev@proton.me>
2024-01-01 04:43:41 +0100

    inyeccion de dependencias

--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -1,16 +1,17 @@
 import { CheckService } from "../domain/useCases/checks/checksService";
 import { CronService } from "./cron/cronService";
 
-
-
-export class Server{
-    public static start(){
-        console.log('Server started...');
-        CronService.createJob('*/5 * * * * *',()=>{
-            new CheckService().execute('https://www.google.com')
-            
-        })
-       
-    }
+export class Server {
+  public static start() {
+    console.log("Server started...");
+    CronService.createJob("*/5 * * * * *", 
+    () => {
+        const url = "https://www.google.com"
+      new CheckService(
+        () => console.log(`${url} is ok`),
+        (error) => console.error(`Error: ${error}`)
+      ).execute(url);
+      // new CheckService().execute('http://localhost:3000') //! Servidor y endpoint con json-Server
+    });
+  }
 }
-
