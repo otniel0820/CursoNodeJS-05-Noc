f2a323bef34ad3c491e3c6bb3a272587ec682e0a
Otniel Lascano dev <otniel.lascano.dev@proton.me>
2024-01-08 23:22:54 +0100

    connect mongodb

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,7 @@
 
 import { envs } from "./config/plugins/envs.plugin";
+import { MongoDatabse } from "./data/mongoDb";
+
 import { Server } from "./presentation/server";
 
 
@@ -7,8 +9,16 @@ import { Server } from "./presentation/server";
   main();
 })();
 
-function main() {
-  Server.start();
+async function main() {
+
+  await MongoDatabse.connect({
+    mongoUrl: envs.MONGO_URL,
+    dbName: envs.MONGO_DB_NAME
+  })
+  
+
+
+  // Server.start();
   // console.log( envs);
   
 }
