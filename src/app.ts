b1053bea8ebc5e9e10a10a457ba8713466dabdca
Otniel Lascano dev <otniel.lascano.dev@proton.me>
2024-01-08 23:55:23 +0100

    create and read with mongo

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,6 @@
 
 import { envs } from "./config/plugins/envs.plugin";
-import { MongoDatabse } from "./data/mongoDb";
+import { LogModel, MongoDatabse } from "./data/mongoDb";
 
 import { Server } from "./presentation/server";
 
@@ -15,8 +15,23 @@ async function main() {
     mongoUrl: envs.MONGO_URL,
     dbName: envs.MONGO_DB_NAME
   })
-  
 
+  // Crear una coleccion = tables(een base de datos relacionales), documento = registro (een base de datos relacionales)
+
+  // const newLog = await LogModel.create({
+  //   message: 'Test Message with mongo',
+  //   origin: 'App.ts',
+  //   level: 'low'
+  // })
+  
+  // await newLog.save()
+
+  // console.log(newLog);
+
+  const logs = await LogModel.find()
+  console.log(logs[1].message);
+  
+  
 
   // Server.start();
   // console.log( envs);
